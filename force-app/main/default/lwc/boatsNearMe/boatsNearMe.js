abe8fed6c581339b2018c05722c44efeb85438ab
Nick Sardonini <nsardonini@umassp.edu>
2022-10-26 17:22:01 -0400

    add boatsNearMe component

--- force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
+++ force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
@@ -1,3 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
+import getBoatsByLocation from "@salesforce/apex/BoatDataService.getBoatsByLocation";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-export default class BoatsNearMe extends LightningElement {}
+const LABEL_YOU_ARE_HERE = "You are here!";
+const ICON_STANDARD_USER = "standard:user";
+const ERROR_TITLE = "Error loading Boats Near Me";
+const ERROR_VARIANT = "error";
+
+export default class BoatsNearMe extends LightningElement {
+  boatTypeId;
+  mapMarkers = [];
+  isLoading = true;
+  isRendered;
+  latitude;
+  longitude;
+
+  // Add the wired method from the Apex Class
+  // Name it getBoatsByLocation, and use latitude, longitude and boatTypeId
+  // Handle the result and calls createMapMarkers
+  @wire(getBoatsByLocation, {
+    latitude: "$latitude",
+    longitude: "$longitude",
+    boatTypeId: "$boatTypeId"
+  })
+  wiredBoatsJSON({ error, data }) {}
+
+  // Controls the isRendered property
+  // Calls getLocationFromBrowser()
+  renderedCallback() {}
+
+  // Gets the location from the Browser
+  // position => {latitude and longitude}
+  getLocationFromBrowser() {}
+
+  // Creates the map markers
+  createMapMarkers(boatData) {
+    // const newMarkers = boatData.map(boat => {...});
+    // newMarkers.unshift({...});
+  }
+}
